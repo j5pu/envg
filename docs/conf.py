2f84f2ceb8c41f6e608a32c9e02954e1edaa5f7e
root <root@example.com>
2023-10-03 19:34:16 +0200

    fix:

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
+
 project = "envg"
 author = "José Antonio Puértolas Montañés"
 copyright = "2023, José Antonio Puértolas Montañés"
@@ -7,5 +11,8 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_click",
 ]
+autodoc_default_flags = ["members"]
 autodoc_typehints = "description"
+toc_object_entries = True
+toc_object_entries_show_parents = "all"
 html_theme = "furo"
