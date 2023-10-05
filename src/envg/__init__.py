1d36785acba731fd7773c18386c64a3ee1fb37b0
root <root@example.com>
2023-10-06 01:03:44 +0200

    auto

--- src/envg/__init__.py
+++ src/envg/__init__.py
@@ -1,7 +1,6 @@
 """envg environment package."""
-from envg import default, env, main, misc, osrelease, secrets, system
+from envg import default, main, misc, osrelease, secrets, system
 from envg.default import *  # noqa: F403
-from envg.env import *  # noqa: F403
 from envg.main import *  # noqa: F403
 from envg.misc import *  # noqa: F403
 from envg.osrelease import *  # noqa: F403
@@ -10,7 +9,6 @@ from envg.system import *  # noqa: F403
 
 __all__ = (
     default.__all__
-    + env.__all__
     + main.__all__
     + misc.__all__
     + osrelease.__all__
